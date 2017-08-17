07237ba02fbfae7f3f136a8a51157dd2b0e1f693
Maxime Morinière <maxime.moriniere@hotmail.fr>
2017-08-17 12:16:02 -0400

    Corrections for PEP8

--- Pol-Tensor_Raman-spectrum/inputfiles.py
+++ Pol-Tensor_Raman-spectrum/inputfiles.py
@@ -7,31 +7,27 @@ import yaml
 import os
 from copy import deepcopy
 
+
 class Input(dict):
     """
-    Class allowing to initialize, read, write and interact with the 
+    Class allowing to initialize, read, write and interact with the
     input file of a BigDFT calculation.
     """
 
     def __init__(self, data):
         """
         An input file is created from a yaml dictionary.
-        
+
         :param data: yaml dictionary of the input file.
         :type data: dict
         """
         for key, value in data.items():
             self[key] = value
-        #if isinstance(data, dict):
-        #    self.data = data
-        #else:
-        #    raise TypeError("The argument must be a (or inherit from) dict.")
-
 
     @classmethod
     def from_file(cls, filename):
         """
-        Initializes the Input instance from a file
+        Initializes the Input instance from a file.
 
         :param filename: Name of the file to read.
         :type filename: str
@@ -42,11 +38,10 @@ class Input(dict):
             data = yaml.load(f)
         return cls(data)
 
-
     @classmethod
     def from_string(cls, string):
         """
-        Initializes the Input instance from a file
+        Initializes the Input instance from a string.
 
         :param string: String of the input file.
         :type string: str
@@ -56,7 +51,6 @@ class Input(dict):
         data = yaml.load(string)
         return cls(data)
 
-
     def write(self, filename):
         """
         Method writing an Input instance in filename.
@@ -65,29 +59,27 @@ class Input(dict):
                          Input instance.
         :type filename: str
         """
-
         with open(filename, "w") as f:
             for key in self:
                 yaml.dump({key: self[key]}, f)
 
 
-
 class Posinp:
     """
-    Class allowing to initialize, read, write and interact with the 
+    Class allowing to initialize, read, write and interact with the
     input geometry of a BigDFT calculation.
     """
 
     def __init__(self, filename):
         """
         A file is read to initialize a Posinp instance.
-        
+
         :param filename: Name of the file to read to initialize the
                          Posinp instance.
         :type filename: str
         """
         with open(filename, "r") as f:
-            # Read the first line, containing the number of atoms and 
+            # Read the first line, containing the number of atoms and
             # the units of the coordinates of each atom
             content = f.readline().split()
             self.n_at = int(content[0])
@@ -101,9 +93,9 @@ class Posinp:
             self.atoms = []
             for i in range(self.n_at):
                 content = f.readline().split()
-                self.atoms.append({"Type": content[0], \
-                    "Position": [float(c) for c in content[1:4]]})
-
+                self.atoms.append({"Type": content[0],
+                                   "Position":
+                                   [float(c) for c in content[1:4]]})
 
     @classmethod
     def from_string(cls, string):
@@ -125,7 +117,6 @@ class Posinp:
         os.remove(tmpfile)
         return posinp
 
-
     def __str__(self):
         """
         Method converting the Posinp instance to a string.
@@ -136,26 +127,24 @@ class Posinp:
         # Create the first two lines of the posinp file
         pos_str = "{}   {}\n".format(self.n_at, self.units)
         pos_str += "{}\n".format(self.BC)
-        # Loop over the atoms to create a line containing their type 
+        # Loop over the atoms to create a line containing their type
         # and coordinates
         for atom in self.atoms:
             pos_str += "{t}   {pos[0]: .15}   {pos[1]: .15}   {pos[2]: .15}\n"\
-            .format(t=atom['Type'], pos=atom['Position'])
+                       .format(t=atom['Type'], pos=atom['Position'])
         return pos_str
-
 
     def __eq__(self, other):
         """
-        Two Posinp instances are the same if their string representation 
-        atoms at the same positions, and use the same units and 
+        Two Posinp instances are the same if their string representation
+        atoms at the same positions, and use the same units and
         boundary conditions.
 
-        :returns: A boolean telling if self and other are the same 
+        :returns: A boolean telling if self and other are the same
                   Posinp instances.
         :rtype: bool
         """
         return str(self) == str(other)
-
 
     def write(self, filename):
         """
@@ -165,19 +154,17 @@ class Posinp:
                          Posinp instance.
         :type filename: str
         """
-
         with open(filename, "w") as f:
             f.write(str(self))
 
-
     def translate_atom(self, i_at, vector):
         """
-        Method returning a new posinp where the atom i_at has been 
-        translated according the vector. 
+        Method returning a new posinp where the atom i_at has been
+        translated according the vector.
 
-        ..Warning: You have to make sure that the units of the vector 
+        ..Warning: You have to make sure that the units of the vector
                    match those used by the posinp.
-        
+
         :param i_at: Index of the atom.
         :type i_at: int
         :param vector: Translation vector to apply.
@@ -185,7 +172,7 @@ class Posinp:
         :returns: A new posinp with the tranlsated atom
         :rtype: posinp.Posinp
         """
-        # Create a deep copy of the posinp and return it after the 
+        # Create a deep copy of the posinp and return it after the
         # update of the position of the atom i_at.
         new_posinp = deepcopy(self)
         for i in range(3):
